8f434063fa56953279a88f00e4ccb769c6e407ab
Chua Bing Quan <chipneverdies@gmail.com>
2022-03-14 16:30:25 +0800

    Remove references to address book in javadocs

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -73,9 +73,9 @@ public class MainApp extends Application {
     }
 
     /**
-     * Returns a {@code ModelManager} with the data from {@code storage}'s address book and {@code userPrefs}. <br>
-     * The data from the sample address book will be used instead if {@code storage}'s address book is not found,
-     * or an empty address book will be used instead if errors occur when reading {@code storage}'s address book.
+     * Returns a {@code ModelManager} with the data from {@code storage}'s LinkyTime and {@code userPrefs}. <br>
+     * The data from the sample LinkyTime will be used instead if {@code storage}'s LinkyTime is not found,
+     * or an empty LinkyTime will be used instead if errors occur when reading {@code storage}'s LinkyTime.
      */
     private Model initModelManager(Storage storage, ReadOnlyUserPrefs userPrefs) {
         ReadOnlyLinkyTime initialData;
