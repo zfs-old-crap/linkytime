10986e6431addf2b7a56c420470c9bdb9f0fb310
Chua Bing Quan <chipneverdies@gmail.com>
2022-03-14 16:24:26 +0800

    Remove sample address book data

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -4,9 +4,7 @@ import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import seedu.address.model.AddressBook;
 import seedu.address.model.LinkyTime;
-import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.ReadOnlyLinkyTime;
 import seedu.address.model.meetingentry.IsRecurring;
 import seedu.address.model.meetingentry.MeetingDateTime;
@@ -14,74 +12,37 @@ import seedu.address.model.meetingentry.MeetingEntry;
 import seedu.address.model.meetingentry.MeetingName;
 import seedu.address.model.meetingentry.MeetingUrl;
 import seedu.address.model.modulecode.ModuleCode;
-import seedu.address.model.person.Address;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Name;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.Phone;
 import seedu.address.model.tag.Tag;
 
 /**
- * Contains utility methods for populating {@code AddressBook} with sample data.
+ * Contains utility methods for populating {@code LinkyTime} with sample data.
  */
 public class SampleDataUtil {
-    public static Person[] getSamplePersons() {
-        return new Person[] {
-            new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                new Address("Blk 30 Geylang Street 29, #06-40"),
-                getTagSet("friends")),
-            new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                getTagSet("colleagues", "friends")),
-            new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                getTagSet("neighbours")),
-            new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                getTagSet("family")),
-            new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                new Address("Blk 47 Tampines Street 20, #17-35"),
-                getTagSet("classmates")),
-            new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                new Address("Blk 45 Aljunied Street 85, #11-31"),
-                getTagSet("colleagues"))
-        };
-    }
-
-    public static ReadOnlyAddressBook getSampleAddressBook() {
-        AddressBook sampleAb = new AddressBook();
-        for (Person samplePerson : getSamplePersons()) {
-            sampleAb.addPerson(samplePerson);
-        }
-        return sampleAb;
-    }
-
     public static MeetingEntry[] getSampleMeetingEntries() {
-        final MeetingUrl dummyMeetingUrl = new MeetingUrl("https://legit-uni.zoom.us/j/344299221?pwd=F3a99221");
         return new MeetingEntry[] {
             new MeetingEntry(
-                    new MeetingName("CS2103T Lecture"),
-                    dummyMeetingUrl,
-                    new MeetingDateTime("18mar20223pm"),
-                    new ModuleCode("CS2103T"),
-                    new IsRecurring("Y"),
-                    getTagSet()
+                new MeetingName("CS2103T Lecture"),
+                new MeetingUrl("https://legit-uni.zoom.us/j/344299221?pwd=F3a99221"),
+                new MeetingDateTime("18mar20223pm"),
+                new ModuleCode("CS2103T"),
+                new IsRecurring("Y"),
+                getTagSet()
             ),
             new MeetingEntry(
-                    new MeetingName("CS2101 Tutorial"),
-                    dummyMeetingUrl,
-                    new MeetingDateTime("19mar20222pm"),
-                    new ModuleCode("CS2101"),
-                    new IsRecurring("Y"),
-                    getTagSet("memes")
+                new MeetingName("CS2101 Tutorial"),
+                new MeetingUrl("https://meet.google.com/omg-look-ma"),
+                new MeetingDateTime("19mar20222pm"),
+                new ModuleCode("CS2101"),
+                new IsRecurring("Y"),
+                getTagSet("memes")
             ),
             new MeetingEntry(
-                    new MeetingName("TokTik Rejection Interview"),
-                    dummyMeetingUrl,
-                    new MeetingDateTime("20mar20221am"),
-                    new ModuleCode("Intern"),
-                    new IsRecurring("N"),
-                    getTagSet("internship", "interview")
+                new MeetingName("TokTik Rejection Interview"),
+                new MeetingUrl("https://www.youtube.com/watch?v=dQw4w9WgXcQ"),
+                new MeetingDateTime("20mar20221am"),
+                new ModuleCode("Intern"),
+                new IsRecurring("N"),
+                getTagSet("internship", "interview")
             )
         };
     }
@@ -102,5 +63,4 @@ public class SampleDataUtil {
                 .map(Tag::new)
                 .collect(Collectors.toSet());
     }
-
 }
